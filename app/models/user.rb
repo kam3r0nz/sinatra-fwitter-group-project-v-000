6e5425de00b8241422d790a51d31c55d032723e5
kam3r0nz <kam.hungerford@gmail.com>
2020-10-27 13:52:57 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+  has_many  :tweets
   has_secure_password
-  has_many :tweets
+  validates_presence_of :username
+  validates_uniqueness_of :username
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
